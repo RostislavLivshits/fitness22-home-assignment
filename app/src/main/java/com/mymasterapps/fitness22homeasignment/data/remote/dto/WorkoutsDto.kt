b640841eef8d1ac905db785d708228961b83686c
Slava Livshits <slava@securepush.com>
2025-09-29 11:11:34 +0300

    Workouts dto

---
+++ app/src/main/java/com/mymasterapps/fitness22homeasignment/data/remote/dto/WorkoutsDto.kt
@@ -0,0 +1,23 @@
+package com.mymasterapps.fitness22homeasignment.data.remote.dto
+
+import com.squareup.moshi.Json
+
+data class WorkoutsFileDto(
+    val workouts: List<WorkoutDayDto>
+)
+
+data class WorkoutDayDto(
+    val day: Int,
+    val workout: List<ExerciseDto>
+)
+
+data class ExerciseDto(
+    @Json(name = "exercise_id") val exerciseId: Int,
+    @Json(name = "exercise_name") val exerciseName: String,
+    @Json(name = "exercise_thumbnail") val exerciseThumbnail: String,
+    @Json(name = "muscle_group") val muscleGroup: String,
+    @Json(name = "muscle_group_image") val muscleGroupImage: String,
+    @Json(name = "amount_of_sets") val amountOfSets: Int,
+    @Json(name = "rep_range") val repRange: String,
+    @Json(name = "weight_amount") val weightAmount: String?
+)
